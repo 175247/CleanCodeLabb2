ed4633563e121c7bb66e2bae5e3dab4a2c270c2d
175247 <yarr-tm@hotmail.com>
2020-11-30 15:19:51 +0100

    Added initial test cases with empty bodies.

---
+++ TollFeeCalculatorTests/TollFeeCalculatorTests.cs
@@ -0,0 +1,28 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TollFeeCalculatorTests
+{
+    [TestClass]
+    public class TollFeeCalculatorTests
+    {
+        [TestMethod]
+        public void DataFile_Should_ThrowException_When_CannotBeFound()
+        {
+        }
+
+        [TestMethod]
+        public void DataFile_Should_ThrowException_When_DoesNotContainDataWithDate()
+        {
+        }
+
+        [TestMethod]
+        public void TollFeePass_Should_ReturnCorrectAmount_When_TimeIsGiven()
+        {
+        }
+
+        [TestMethod]
+        public void CheckIsFreePass_Should_ReturnCorrectBool_When_DateIsGiven()
+        {
+        }
+    }
+}
