47b96137fe7390cb933bcb52f4044e167a1221f8
175247 <yarr-tm@hotmail.com>
2020-12-07 21:54:29 +0100

    Removed unused property (InvalidFilePath).

--- TollFeeCalculatorTests/SettingsMock.cs
+++ TollFeeCalculatorTests/SettingsMock.cs
@@ -5,6 +5,5 @@ namespace TollFeeCalculatorTests
     public class SettingsMock
     {
         public string DataFilePath { get; } = Environment.CurrentDirectory + "../../../../../TollFeeCalculator/testData.txt";
-        public string InvalidDataFilePath { get; } = Environment.CurrentDirectory + "../../../../../TollFeeCalculator/enFinKo.txt";
     }
 }
